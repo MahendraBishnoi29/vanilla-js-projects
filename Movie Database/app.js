03231ca67ed46901d5a80f1631010f55be2118b8
Mahendra <mahendrabrowny@gmail.com>
2022-10-05 14:04:00 +0530

    valdating submit form 🔑

--- Movie Database/app.js
+++ Movie Database/app.js
@@ -1,6 +1,9 @@
 const addMovieModal = document.getElementById("add-modal");
 const startAddMovie = document.querySelector("header button");
 const backdrop = document.getElementById("backdrop");
+const cancel = addMovieModal.querySelector(".btn--passive");
+const confirm = cancel.nextElementSibling;
+const userInputs = document.querySelectorAll("input");
 
 const showBackdrop = () => {
   backdrop.classList.toggle("visible");
@@ -11,9 +14,23 @@ const toggleModalClass = () => {
   showBackdrop();
 };
 
-const handleCloseModal = () => {
+const handleClose = () => {
   toggleModalClass();
 };
 
+const handleSubmit = () => {
+  const textInputs = userInputs[0].value;
+  const imgInput = userInputs[1].value;
+  const ratingInput = userInputs[2].value;
+
+  if (!textInputs.trim() || !imgInput.trim() || +ratingInput < 1) {
+    alert("Please fill all the fields!");
+  } else if (+ratingInput > 5) {
+    alert("Rating should less then 5!");
+  }
+};
+
 startAddMovie.addEventListener("click", toggleModalClass);
-backdrop.addEventListener("click", handleCloseModal);
+backdrop.addEventListener("click", handleClose);
+cancel.addEventListener("click", handleClose);
+confirm.addEventListener("click", handleSubmit);
